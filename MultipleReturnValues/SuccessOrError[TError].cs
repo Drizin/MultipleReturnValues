d43f6344f49eaa439c0eeafec69ba793c4df4a75
Rick Drizin <Drizin@users.noreply.github.com>
2021-01-09 22:13:27 -0300

    SuccessOrError<TError> (if doesn't return any other type besides the possible error)

---
+++ MultipleReturnValues/SuccessOrError[TError].cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace MultipleReturnValues
+{
+
+    /// <summary>
+    /// Returns the result of a command that does NOT output any entity, but may output an error (enum)
+    /// </summary>
+    [DebuggerDisplay("{Error == null ? \"Success\" : Error,nq}")]
+    public class SuccessOrError<TError> : SuccessOrErrorBase<TError>
+        where TError : struct, Enum
+    {
+        #region Members
+        #endregion
+
+        #region ctor
+        public static SuccessOrError<TError> ValidationError(IList<ValidationError> validationErrors, string errorMessage = "Validation Error")
+        {
+            return new SuccessOrError<TError>() { ErrorResult = new ErrorResult<TError>(validationErrors, errorMessage) };
+        }
+        public static SuccessOrError<TError> Fail(TError errorCode, string errorMessage = null)
+        {
+            return new SuccessOrError<TError>() { ErrorResult = new ErrorResult<TError>(errorCode, errorMessage) };
+        }
+        public static SuccessOrError<TError> Success(string successMessage = "Success")
+        {
+            return new SuccessOrError<TError>() { SuccessMessage = successMessage };
+        }
+        #endregion
+
+        #region Implicit conversions from/to TError
+        public static implicit operator SuccessOrError<TError>(TError? error)
+        {
+            //if you do "return (null)", it will be converted to a Success(). "return TError.SomeValue" will be converted to a Fail()
+            if (error == null)
+                return Success();
+            else
+                return Fail(error.Value);
+        }
+        public static implicit operator TError?(SuccessOrError<TError> result)
+        {
+            return result?.ErrorResult.ErrorCode;
+        }
+        #endregion
+
+    }
+}
